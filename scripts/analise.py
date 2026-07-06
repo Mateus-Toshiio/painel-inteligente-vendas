a354c363505be4466f24512f5756943ab01f8019
Mateus Toshio <mateustoshio48@gmail.com>
2026-07-06 18:37:22 -0300

    Código organizado com adição prévia do README

--- scripts/analise.py
+++ scripts/analise.py
@@ -8,15 +8,38 @@ def carregar_dados():
     return df
 
 
+def faturamento_total(df):
+    return df["Valor"].sum()
+
+def quantidade_vendas(df):
+    return len(df)
+
+def maior_valor_venda(df):
+    return df["Valor"].max()
+
+def menor_valor_venda(df):
+    return df["Valor"].min()
+
+def valor_medio_venda(df):
+    return df["Valor"].mean()
+
+def ranking_vendedores_por_faturamento(df):
+    return df.groupby("Vendedor")["Valor"].sum().sort_values(ascending=False)
+
+def ranking_produtos_mais_vendidos(df):
+    return df.groupby("Produto")["Quantidade"].sum().sort_values(ascending=False)
+
 def resumo_geral(df):
     """Calcula as principais métricas."""
 
     resumo = {
-        "quantidade_vendas": len(df),
-        "faturamento_total": df["Valor"].sum(),
-        "maior_venda": df["Valor"].max(),
-        "menor_venda": df["Valor"].min(),
-        "media_vendas": df["Valor"].mean(),
+        "quantidade_vendas": quantidade_vendas(df),
+        "faturamento_total": faturamento_total(df),
+        "maior_valor_venda": maior_valor_venda(df),
+        "menor_valor_venda": menor_valor_venda(df),
+        "valor_medio_venda": valor_medio_venda(df),
+        "ranking_vendedores_por_faturamento": ranking_vendedores_por_faturamento(df).head(3),
+        "ranking_produtos_mais_vendidos": ranking_produtos_mais_vendidos(df).head(3)
     }
 
     return resumo
--- scripts/main.py
+++ scripts/main.py
@@ -8,8 +8,22 @@ df = carregar_dados()
 
 resumo = resumo_geral(df)
 
+posicao = 1
+
+
 print(f"\nQuantidade de vendas: {resumo['quantidade_vendas']}")
 print(f"Faturamento total: R$ {resumo['faturamento_total']:,.2f}")
-print(f"Maior venda: R$ {resumo['maior_venda']:,.2f}")
-print(f"Menor venda: R$ {resumo['menor_venda']:,.2f}")
-print(f"Média das vendas: R$ {resumo['media_vendas']:,.2f}")
+print(f"Maior venda: R$ {resumo['maior_valor_venda']:,.2f}")
+print(f"Menor venda: R$ {resumo['menor_valor_venda']:,.2f}")
+print(f"Média das vendas: R$ {resumo['valor_medio_venda']:,.2f}")
+
+print("\nProdutos mais vendidos:\n")
+for produto, quantidade in resumo['ranking_produtos_mais_vendidos'].items():
+    print(f"{posicao}° {produto} - {quantidade}")
+    posicao += 1
+
+print("\nRanking de Vendedores\n")
+posicao = 1
+for vendedor, valor in resumo["ranking_vendedores_por_faturamento"].items():
+    print(f"{posicao}° {vendedor} - R$ {valor:,.2f}")
+    posicao += 1
