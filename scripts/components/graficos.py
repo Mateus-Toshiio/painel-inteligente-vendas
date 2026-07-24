4d3eff47dc279b6e90934d8c9687e456c7d7b5ba
Toshiio <mateustoshio48@gmail.com>
2026-07-24 15:40:54 -0300

    Alterada ordenação do grafico e resolvido o bug do grafico

--- scripts/components/graficos.py
+++ scripts/components/graficos.py
@@ -9,6 +9,17 @@ def mostrar_graficos(faturamento_por_produto, faturamento_por_vendedor):
                      x="Valor",
                      y="Produto",
                      title="Faturamento por Produto")
+        
+        fig.update_traces(texttemplate="R$ %{x:,.2f}",
+                          textposition="outside")
+        
+        fig.update_layout(margin=dict(r=80),
+                          xaxis_title="",
+                          yaxis_title="",
+                          xaxis_range=[0, faturamento_por_produto["Valor"].max() * 1.2,],
+                          )
+        
+        fig.update_yaxes(autorange="max reversed")
 
         st.plotly_chart(fig,
                         use_container_width=True)
